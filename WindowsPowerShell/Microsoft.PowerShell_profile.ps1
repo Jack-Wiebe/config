c9714e7233af8c4d3b74f7214a9e131b7aaf2b9c
JackWiebe <jackw@flutterstudios.com>
2024-06-03 19:05:10 -0400

    feat: powershell profile

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jawa.omp.json" | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
+Function mvnci { mvn clean install -DskipTests }
+
+Function gitc { git clean -df }
+
+Function gitr { git reset --hard }
+
+Function gits { git status }
+
+Function gitd { git diff }
+
+function gitx {
+	gitc
+	gitr
+	gits	
+}
